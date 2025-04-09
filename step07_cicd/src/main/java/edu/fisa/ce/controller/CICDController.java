a072cf87082f292e3f02ff0996677c72dcf27cc7
kcklkb <kchaklkb@naver.com>
2025-04-09 09:45:49 +0900

    [UPDATE] test src

--- step07_cicd/src/main/java/edu/fisa/ce/controller/CICDController.java
+++ step07_cicd/src/main/java/edu/fisa/ce/controller/CICDController.java
@@ -1,14 +1,23 @@
 package edu.fisa.ce.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j //lombok 내부에 있는 완전 추상화 극대화된 로그library
 @RestController
 public class CICDController {
-
+		
+		//http://localhost:8080/fisa1
 		@GetMapping("fisa1")
 		public String reqRes() {
 			System.out.println("reqRes() *******");
+			log.info("***요청 & 응답 ***");
+			for(int i=1; i<=10; i++) {
+				System.out.println("data 값" + i);
+			}
 			return "요청 응답 성공";
 		}
 		
--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,12 +2,23 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+		@Test
+		public void testAddition() {
+			int result = 2 + 3;
+			/* 예시 -> 서버가 시작될때 특정 db로부터 검색을 해옴
+			 * 그 데이터는 반듯이 fisa여야만 하는 상황
+			 * 만약 fisa가 없다면 서버 중지 또는 다른 로직 실행 유도 등의 액션을 취함
+			 * 서비스 전에 사전 검증하는 것
+			 * 
+			 * 예시2 -> 개발중에 데이터에 대한 test 검증용등으로 사용
+			 */
+			System.out.println("비교전*************");
+			assertEquals(5, result); // 성공: 5 == 5
+			System.out.println("비교후*************");
+		}
 }
